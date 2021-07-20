6fd379eed379d857994eeee78c932ffdd48a2644
P Joser Atauchi <patauchi@Ps-MacBook-Pro.local>
2021-07-20 16:24:34 -0500

    v 1.0.1 create outdir

--- R/gwHeterogeneity.R
+++ R/gwHeterogeneity.R
@@ -87,8 +87,9 @@ gwHeterogeneity <- function(layers, factor, parallel=TRUE, cores=4, save=FALSE, 
   
 
   if(save){
+    if(!dir.exists(Hetero_base)){dir.create(Hetero_base)}
     terra::writeRaster(Hetero_base, 
-                       paste0(outdir, '/Heterogeneity_',(resIn * factor),'_res.tif'))
+                       paste0(outdir, '/Hetero_', factor,'.tif'))
     
   } else{
    plot(Hetero_base) 
