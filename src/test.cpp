28c16946b1a13747ca2c35211a5a10a819fb1e75
P Joser Atauchi <patauchi@Ps-MacBook-Pro.local>
2021-07-23 21:14:37 -0500

    v 1.0.1

--- src/test.cpp
+++ src/test.cpp
@@ -11,7 +11,7 @@ using namespace Rcpp;
 using namespace arma;
 using namespace stats;
 
-double sp_dists_pp(double lon1, double lon2, double lat1, double lat2, double tau) {
+double sp_dists_pp2(double lon1, double lon2, double lat1, double lat2, double tau) {
   double as1 = ((lon2 - lon1 ) * (lon2 - lon1 )) + ((lat2 - lat1 ) * (lat2 - lat1 ));
   double as2 = std::exp(-as1 / (2 * (tau * tau)));
   return as2;
@@ -26,7 +26,7 @@ arma::mat gw_hetero_rmpi(arma::mat coodXY, arma::vec pointXY, arma::mat Y, doubl
   double uout = pointXY(0), vout = pointXY(1);
   for (j = 0; j < N; j++) {
     
-    w(j) = sp_dists_pp(coodXY(j, 0), uout, coodXY(j, 1), vout, tau);
+    w(j) = sp_dists_pp2(coodXY(j, 0), uout, coodXY(j, 1), vout, tau);
     
   }
 
